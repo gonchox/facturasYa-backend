b334cd56051c9fd50dbcd0742da77d4a57860e3c
Sebastian <sebastian.rv.2001@gmail.com>
2021-06-04 18:27:13 -0500

    Primera version del proyecto

---
+++ src/main/java/com/appfacturasya/AppfacturasyaApplication.java
@@ -0,0 +1,13 @@
+package com.appfacturasya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfacturasyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppfacturasyaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appfacturasya/AppfacturasyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appfacturasya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfacturasyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
